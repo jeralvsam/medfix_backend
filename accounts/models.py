631514bad5b9316771f07e7b41c4f01dec7082ad
Jeral V Sam <jeralvsam@gmail.com>
2026-04-13 14:13:46 +0530

    Added Ticket model

--- accounts/models.py
+++ accounts/models.py
@@ -10,3 +10,21 @@ class User(AbstractUser):
 
     def __str__(self):
         return self.username
+class Ticket(models.Model):
+    STATUS_CHOICES = (
+        ('PENDING', 'Pending'),
+        ('RESOLVED', 'Resolved'),
+    )
+
+    computer_id = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+    description = models.TextField()
+
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='PENDING')
+
+    created_by = models.ForeignKey(User, on_delete=models.CASCADE, related_name='tickets')
+
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.computer_id} - {self.status}"
